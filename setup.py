d0d67deb7014be445d7f47d95bd128b6e2191491
Valter Iversen Fernandes de Souza <iversen.souza@gmail.com>
2025-11-08 23:12:03 -0300

    Ajuste do nome do autor

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="robotframework-androiduiautomation",
-    version="0.1.7",
+    version="0.1.8",
     description="Robot Framework library for Android automation using uiautomator2",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    author="Seu Nome",
+    author="Valter I F de Souza",
     packages=find_packages(),
     py_modules=["AndroidUiAutomation"], 
     install_requires=[
